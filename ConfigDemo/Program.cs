3dc845de4efc3ab2a96215aa8a4e002fab18c8d3
yanxi zhu <547110356@qq.com>
2019-08-29 20:40:41 +0800

    add appsettings.json change detect

--- ConfigDemo/Program.cs
+++ ConfigDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Memory;
+using Microsoft.Extensions.Primitives;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,45 +12,13 @@ namespace ConfigDemo
     {
         private static void Main(string[] args)
         {
-            //var source = new Dictionary<string, string>
-            //{
-            //    ["longDatePattern"] = "dddd, MMMM d, yyyy",
-            //    ["longTimePattern"] = "h:mm:ss tt",
-            //    ["shortDatePattern"] = "M/d/yyyy",
-            //    ["shortTimePattern"] = "h:mm tt"
-            //};
-            //IConfigurationRoot config = new ConfigurationBuilder()
-            //    .Add(new MemoryConfigurationSource { InitialData = source })
-            //    .Build();
-            //var options = new DateTimeFormatOptions(config);
+            VersionOne();
+            VersionTwo();
+        }
 
-            var source = new Dictionary<string, string>
-            {
-                ["format:dateTime:longDatePattern"] = "dddd, MMMM d, yyyy",
-                ["format:dateTime:longTimePattern"] = "h:mm:ss tt",
-                ["format:dateTime:shortDatePattern"] = "M/d/yyyy",
-                ["format:dateTime:shortTimePattern"] = "h:mm tt",
-
-                ["format:currencyDecimal:digits"] = "2",
-                ["format:currencyDecimal:symbol"] = "$",
-            };
-
-            //var configuration = new ConfigurationBuilder()
-            //    .Add(new MemoryConfigurationSource { InitialData = source })
-            //    .Build();
-            //var options = new FormatOptions(configuration.GetSection("Format"));
-
-            var currentPath = Directory.GetCurrentDirectory();//此方法获取的是dotnet命令运行的根目录,所以调试时会得到~\bin\Debug\netcoreapp2.2
-
-#if DEBUG
-            // 调试用代码
-            Console.WriteLine("CurrentPath1:" + currentPath);
-            var pathArr = currentPath.Split('\\').ToList();
-            int count = pathArr.Count;
-            pathArr.RemoveRange(count - 3, 3);
-            currentPath = string.Join('\\', pathArr);
-            Console.WriteLine("CurrentPath2:" + currentPath);
-#endif
+        private static void VersionOne()
+        {
+            var currentPath = CurrentPath();
 
             var options = new ConfigurationBuilder()
                 //.Add(new MemoryConfigurationSource { InitialData = source })//当添加了“appsettings.json”配置文件后就应当从配置文件读取数据源
@@ -62,7 +31,7 @@ namespace ConfigDemo
             var dateTime = options.DateTime;
             var currencyDecimal = options.CurrencyDecimal;
 
-            Console.WriteLine("**************************************************");
+            Console.WriteLine("*****************Start Version One*****************");
             Console.WriteLine("DateTime:");
             Console.WriteLine($"\tLongDatePattern: {dateTime.LongDatePattern}");
             Console.WriteLine($"\tLongTimePattern: {dateTime.LongTimePattern}");
@@ -73,7 +42,54 @@ namespace ConfigDemo
             Console.WriteLine($"\tDigits:{currencyDecimal.Digits}");
             Console.WriteLine($"\tSymbol:{currencyDecimal.Symbol}");
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("*****************End Version One*****************");
+            Console.WriteLine("");
+        }
+
+        private static void VersionTwo()
+        {
+            var currentPath = CurrentPath();
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(currentPath)
+                .AddJsonFile(path: "appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+            ChangeToken.OnChange(() => config.GetReloadToken(), () =>
+            {
+                var options = config.GetSection("format").Get<FormatOptions>();
+                var dateTime = options.DateTime;
+                var currencyDecimal = options.CurrencyDecimal;
+
+                Console.WriteLine("*****************Start Version Two*****************");
+                Console.WriteLine("DateTime:");
+                Console.WriteLine($"\tLongDatePattern: {dateTime.LongDatePattern}");
+                Console.WriteLine($"\tLongTimePattern: {dateTime.LongTimePattern}");
+                Console.WriteLine($"\tShortDatePattern: {dateTime.ShortDatePattern}");
+                Console.WriteLine($"\tShortTimePattern: {dateTime.ShortTimePattern}");
+
+                Console.WriteLine("CurrencyDecimal:");
+                Console.WriteLine($"\tDigits:{currencyDecimal.Digits}");
+                Console.WriteLine($"\tSymbol:{currencyDecimal.Symbol}\n\n");
+                Console.WriteLine("*****************End Version Two*****************");
+                Console.WriteLine("");
+            });
+            Console.ReadKey();
+        }
+
+        private static string CurrentPath()
+        {
+            var currentPath = Directory.GetCurrentDirectory();//此方法获取的是dotnet命令运行的根目录,所以调试时会得到~\bin\Debug\netcoreapp2.2
+
+#if DEBUG
+            // 调试用代码
+            Console.WriteLine("CurrentPath1:" + currentPath);
+            var pathArr = currentPath.Split('\\').ToList();
+            int count = pathArr.Count;
+            pathArr.RemoveRange(count - 3, 3);
+            currentPath = string.Join('\\', pathArr);
+            Console.WriteLine("CurrentPath2:" + currentPath);
+#endif
+            return currentPath;
         }
     }
 }
