6af3201c7b83605bc7462328cfcde0892489081f
yanxi zhu <547110356@qq.com>
2019-10-24 23:48:32 +0800

    no comment

--- CodeInterview/04_FindInPartiallySortedMatrix.cs
+++ CodeInterview/04_FindInPartiallySortedMatrix.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace CodeInterview
 {
+    // 题目：在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按
+    // 照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个
+    // 整数，判断数组中是否含有该整数。
     //[TestFixture]
     public class FindInPartiallySortedMatrix
     {
