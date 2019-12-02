dcad607ec97fae9848e4996e8477dfe9102d0972
zhuyanxi <547110356@qq.com>
2019-12-02 21:18:01 +0800

    add graphql demo

---
+++ GraphQLDemo/FakeData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using GraphQL;
+using GraphQL.Types;
+
+namespace GraphQLDemo
+{
+    public class Droid
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class DroidType : ObjectGraphType<Droid>
+    {
+        public DroidType()
+        {
+            Field(x => x.Id).Description("The Id of the Droid.");
+            Field(x => x.Name).Description("The name of the Droid.");
+        }
+    }
+
+    public class Query
+    {
+        [GraphQLMetadata("hero")]
+        public Droid GetHero() => new Droid { Id = "1", Name = "R2-D2" };
+        //{
+        //    return new Droid { Id = "1", Name = "R2-D2" };
+        //}
+    }
+
+    public class StarWarsQuery : ObjectGraphType
+    {
+        public StarWarsQuery()
+        {
+            Field<DroidType>(
+              "hero",
+              resolve: context => new Droid { Id = "1", Name = "R2-D2" }
+            );
+        }
+    }
+}
---
+++ GraphQLDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using GraphQL;
+using GraphQL.Types;
+
+namespace GraphQLDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var schema = Schema.For(@"
+            //  type Query {
+            //    hello: String
+            //  }
+            //");
+            //var json = schema.Execute(_ =>
+            //{
+            //    _.Query = "{ hello }";
+            //    _.Root = new { Hello = "Hello World!" };
+            //});
+
+
+            //var schema = Schema.For(@"
+            //  type Droid {
+            //    id: String!
+            //    name: String!
+            //  }
+
+            //  type Query {
+            //    hero: Droid
+            //  }
+            //", _ => {
+            //    _.Types.Include<Query>();
+            //});
+            //var json = schema.Execute(_ =>
+            //{
+            //    _.Query = "{ hero { id name } }";
+            //});
+
+
+            var schema = new Schema { Query = new StarWarsQuery() };
+            var json = schema.Execute(_ =>
+            {
+                _.Query = "{ hero { id name } }";
+            });
+
+            Console.WriteLine(json);
+            Console.ReadKey();
+        }
+    }
+}
